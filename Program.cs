94be2faa6f211015c4b0b78aa54cfd6b9ce0b2bd
SatishR <sramah@gmail.com>
2019-03-22 13:41:35 -0700

    Modified message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace democonsoleapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Installed GIT");
         }
     }
 }
